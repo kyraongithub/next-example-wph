742624b3597d1e6a2bef6151beee5344b98921fd
Kyra <ramadhanky68@gmail.com>
2025-06-16 21:27:58 +0700

    feat/update home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <h1>hello world</h1>
+      <h1>hello world update</h1>
       {/* <img
         src="https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM="
         alt=""
